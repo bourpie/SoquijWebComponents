9702344119e1a76d2017872dabc221b3b08751e0
pbourgeois <pbourgeois@soquij.qc.ca>
2023-08-17 07:53:42 -0400

    couleurs du theme

--- .storybook/yourTheme.ts
+++ .storybook/yourTheme.ts
@@ -6,4 +6,30 @@ export default create({
   brandUrl: '/',
   brandImage: 'https://aristote.soquij.qc.ca/a/media/logos/sqj-headerlogo.svg',
   brandTarget: '_self',
+
+//
+colorPrimary: '#3A10E5',
+colorSecondary: '#585C6D',
+
+// UI
+appBg: '#ffffff',
+appContentBg: '#ffffff',
+appBorderColor: '#585C6D',
+appBorderRadius: 4,
+
+// Text colors
+textColor: '#10162F',
+textInverseColor: '#ffffff',
+
+// Toolbar default and active colors
+barTextColor: '#9E9E9E',
+barSelectedColor: '#585C6D',
+barBg: '#ffffff',
+
+// Form colors
+inputBg: '#ffffff',
+inputBorder: '#10162F',
+inputTextColor: '#10162F',
+inputBorderRadius: 2,
+
 });
