aa92503a83c44de913d386f521b88db094ecdcb0
pbourgeois <pbourgeois@soquij.qc.ca>
2023-08-17 07:47:31 -0400

    maj et creation d'un theme storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import yourTheme from './yourTheme';
+
+addons.setConfig({
+  theme: yourTheme,
+});
---
+++ .storybook/yourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'UI SOQUIJ',
+  brandUrl: '/',
+  brandImage: 'https://aristote.soquij.qc.ca/a/media/logos/sqj-headerlogo.svg',
+  brandTarget: '_self',
+});
